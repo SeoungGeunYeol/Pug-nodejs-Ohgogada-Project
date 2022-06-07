9252b0df5fc5f5c0fb57f302c92a57cb4aa9ab0c
SeoungGeunYeol <swaq11@naver.com>
2022-06-07 20:46:52 +0900

    gihub Login finish

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,8 @@
 import User from "../models/User";
+import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 
-// globalRouter
+// ** globalRouter **
 export const getSignup = (req, res) =>
   res.render("signup", { pageTitle: "Sign Up" });
 export const postSignup = async (req, res) => {
@@ -42,7 +43,7 @@ export const getLogin = (req, res) =>
 export const postLogin = async (req, res) => {
   const { email, password } = req.body;
   const pageTitle = "Log In";
-  const user = await User.findOne({ email });
+  const user = await User.findOne({ email, socialOnly: false });
   // check if account exists
   if (!user) {
     return res.status(400).render("login", {
@@ -63,7 +64,95 @@ export const postLogin = async (req, res) => {
   res.redirect("/");
 };
 
-// userRouter
+// ** userRouter **
+// getGithubLogin (memo.txt 참조)
+export const getGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: process.env.GH_CLIENTID,
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
+
+/*
+fetch()  함수
+-- HTTP 요청 전송 기능을 제공하는 Wen API이다.(JS내장 라이브러리)
+-- Server와 비동기 요청 방식을 기능한다.
+-- 
+1. fetch('url')로 다른 서버를 통해 데이터를 가져올 수 있다.
+but res.body 에 담겨있는 url은 제대로 된 객체를 받아올 수 없다.
+2. json함수가 res의 스트림을 가져와 끝까지 읽고, res.body의 텍스트를 promise형태로
+반환한다.
+3. 다른 서버의 데이터를 object형식으로 받아온다.
+*/
+
+export const postGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENTID,
+    client_secret: process.env.GH_CLIENTID_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const tokenRequest = await (
+    await fetch(finalUrl, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+  ).json();
+  if ("access_token" in tokenRequest) {
+    // access api
+    const { access_token } = tokenRequest;
+    const apiUrl = "https://api.github.com";
+    const userData = await (
+      await fetch(`${apiUrl}/user`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    const emailData = await (
+      await fetch(`${apiUrl}/user/emails`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    const emailObj = emailData.find(
+      (email) => email.primary === true && email.verified === true
+    );
+    if (!emailObj) {
+      return res.redirect("/login");
+    }
+    let user = await User.findOne({ email: emailObj.email });
+    // create user
+    if (!user) {
+      user = await User.create({
+        name: userData.name,
+        avatarUrl: userData.avatar_url,
+        email: emailObj.email,
+        username: userData.login,
+        password: "",
+        socialOnly: true,
+        location: userData.location,
+      });
+    }
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+// log Out session.destroy()
 export const logout = (req, res) => {
   req.session.destroy();
   return res.redirect("/");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,20 @@
 import express from "express";
-import { edit, remove, logout, detail } from "../controllers/userController";
+import {
+  edit,
+  remove,
+  logout,
+  detail,
+  getGithubLogin,
+  postGithubLogin,
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
+userRouter.get("/github/start", getGithubLogin);
+userRouter.get("/github/finish", postGithubLogin);
 userRouter.get("/:id", detail);
 
 export default userRouter;
